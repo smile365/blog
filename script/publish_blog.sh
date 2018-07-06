2ebde94ae6288b168ce80648d96dac1b28883929
宋学彦 <songxueyan@wordemotion.com>
2018-07-06 12:36:37 +0800

    edit config

--- script/publish_blog.sh
+++ script/publish_blog.sh
@@ -1,10 +1,12 @@
 #!/usr/bin/sh
 #publish_blog.sh
-rm -rf /home/myblog/content/post*
+rm -rf /home/myblog/content/post/*
 cd /home/script/blog
 echo `pwd`
 echo `git pull`
-cp -rf *.md /home/myblog/content/post
+cp -rf *.md /home/myblog/content/post/
+cp -rf nuo /home/myblog/themes/nuo/static/
+cp -f script/nuo.toml /home/myblog/config.toml
 cd /home/myblog
 echo `pwd`
 echo `hugo -b https://sxy91.com/`
