97b2964c74c3c874d93d6bdd2930381ae4b5b650
宋学彦 <songxueyan@wordemotion.com>
2018-07-06 20:27:03 +0800

    remove theme files

--- script/publish_blog.sh
+++ script/publish_blog.sh
@@ -6,10 +6,9 @@ echo `pwd`
 echo `git pull`
 
 cp -rf *.md /home/myblog/content/post/
-cp -rf nuo/* /home/myblog/themes/nuo/static/
+#cp -rf nuo/* /home/myblog/themes/nuo/static/
 cp -f script/nuo.toml /home/myblog/config.toml
 cd /home/myblog
 rm -rf public
 echo `pwd`
-echo `hugo -b http://sxy91.com/`
 echo `hugo -b https://sxy91.com/`
