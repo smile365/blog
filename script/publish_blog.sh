d698f2a8901f1b37aa8b42ba339c06cd3dddd0b2
宋学彦 <songxueyan@wordemotion.com>
2018-07-07 15:34:42 +0800

    script

--- script/publish_blog.sh
+++ script/publish_blog.sh
@@ -1,14 +1,15 @@
 #!/usr/bin/sh
 #publish_blog.sh
-rm -rf /home/myblog/content/post/*
-cd /home/script/blog
-echo `pwd`
-echo `git pull`
-
+# rm -rf /home/myblog/content/post/*
+# cd /home/script/blog
+# echo `pwd`
+# echo `git pull`
+# echo `./publish_blog.sh`
 cp -rf *.md /home/myblog/content/post/
-#cp -rf nuo/* /home/myblog/themes/nuo/static/
-cp -f script/nuo.toml /home/myblog/config.toml
-cd /home/myblog
+cd /home/myblog/themes/nuo
+echo `git pull`
+cp -f exampleSite/config.toml /home/myblog/
+cd /home/myblog/
 rm -rf public
 echo `pwd`
 echo `hugo -b https://sxy91.com/`
